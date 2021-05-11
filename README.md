a71090cdcb6943fabf7667a8a5066ce72050bd46
Aditya <ag15035@gmail.com>
2021-05-11 14:07:53 +0530

    Add Comments & README

---
+++ README.md
@@ -0,0 +1,13 @@
+## Linux From Scratch
+
+Linux From Scratch is an awesome book, thanks to the community for maintaining it.
+
+Read/Download from https://www.linuxfromscratch.org/lfs/
+
+Agar aap pehli baar LFS system build kar rahe ho to, don't use the automatic scripts except the downloader script. Build it yourself, it will be worth the time 😁
+
+### Scripts
+
+* `smart_download.sh` - The LFS book has list of packages, their hashes and download url, just copy that part to a `lfs_package.txt` file, and run the script, that's it, you may change the download location by modifying the `source_directory` variable in the script
+
+Thanks to all the maintainers of LFS ❣
--- smart_download.sh
+++ smart_download.sh
@@ -1,5 +1,12 @@
-export source_directory="./"
+# TODO- Add support for incompletely downloaded files, by veryfing the MD5 hash
 
+export source_directory="sources/"
+
+# This function takes the download url for a package as argument
+# Then, it extracts the filename from download url, storing it in `name`
+# Then, it checks if a file with that name exists in the source_directory
+# If exists, then skip
+# Else download the file
 fn() {
     url=$1
     name=$(echo ${url} | rev | awk -F '/' '{print $1}' | rev)
@@ -10,13 +17,17 @@ fn() {
         echo $source_directory$name" exists"
     else
         echo "Downloading "$source_directory$name
-        wget --continue $url -P $source_directory -q
+        wget --continue $url -P $source_directory -q --show-progress
     fi
 
     # echo $name
 }
 
 export -f fn
-awk '{print}' urls.txt | xargs -P 0 -I {} bash -c 'fn "$@"' _ {}
+mkdir -pv $source_directory     # create the source directory if doesn't exist yet (wget would automatically create either way)
+
+# This line first extracts all download urls from lfs_packages.txt, then executes `fn` parallely for each download url
+awk '/Download: / {print $2}' lfs_packages.txt | xargs -P 0 -I {} bash -c 'fn "$@"' _ {}
 
 unset source_directory
+unset fn
